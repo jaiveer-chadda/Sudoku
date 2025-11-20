4ffcc301511112126d6074a2cd6fbd7350dc4872
JV <jaiveer.chadda@gmail.com>
2025-11-20 20:57:52 +0000

    Commented everything out of main.py and utils.py, so I can start again - the code was all too mangled to be usable

--- source/utils.py
+++ source/utils.py
@@ -1,34 +1,38 @@
-from typing import Literal
 
 
-def is_solved(bv: dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]]) -> bool:
-    return all(map(lambda x: x[2], bv.values()))
 
 
-def get_candidates(
-        bv:  dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]],
-        key: tuple[int, int]
-) -> set[int]:
-    visible = bv[key][3]
-    return set(range(1, 10)) - visible
-
-
-def add_seeable_vals(
-        board_values: dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]],
-        loc: tuple[int, int],
-        to_add: Literal['candidates', 'value'] = 'value'
-):
-    pass
-
-
-def print_board(raw_board: list[list[int]]) -> None:
-    print()
-    for row in range(len(raw_board)):
-        for col in range(len(raw_board[row])):
-            print(raw_board[row][col] if raw_board[row][col] != 0 else " ", end=" ")
-        print()
-    print()
-
-
-def print_line() -> None:
-    print("-"*18)
+# from typing import Literal
+#
+#
+# def is_solved(bv: dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]]) -> bool:
+#     return all(map(lambda x: x[2], bv.values()))
+#
+#
+# def get_candidates(
+#         bv:  dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]],
+#         key: tuple[int, int]
+# ) -> set[int]:
+#     visible = bv[key][3]
+#     return set(range(1, 10)) - visible
+#
+#
+# def add_seeable_vals(
+#         board_values: dict[tuple[int, int], list[int | tuple[int, int] | bool | set[int]]],
+#         loc: tuple[int, int],
+#         to_add: Literal['candidates', 'value'] = 'value'
+# ):
+#     pass
+#
+#
+# def print_board(raw_board: list[list[int]]) -> None:
+#     print()
+#     for row in range(len(raw_board)):
+#         for col in range(len(raw_board[row])):
+#             print(raw_board[row][col] if raw_board[row][col] != 0 else " ", end=" ")
+#         print()
+#     print()
+#
+#
+# def print_line() -> None:
+#     print("-"*18)
