0fc683e0f7f27e14b70b72b3b04e95c74b193f5c
JV <jaiveer.chadda@gmail.com>
2025-11-25 01:15:54 +0000

    Addd an alternate 'type workaround' to types_.py

--- source/common/types_.py
+++ source/common/types_.py
@@ -1,9 +1,13 @@
 #—— External Imports —————————————————————————————————————————————————————————————————————————
-from typing import TypeVar, Literal, Iterable
+from typing import TypeVar, Literal, Iterable  #, Annotated
+
 #—————————————————————————————————————————————————————————————————————————————————————————————
 
 
 T: TypeVar = TypeVar('T')
+
+#_ Cell_: TypeVar = TypeVar('Cell_')
+#_ Board_: TypeVar = TypeVar('Board_')
 
 type colour = int
 type coordinates = tuple[int, int]
@@ -15,6 +19,9 @@ type cell_insert_type = Literal["value", "corner", "centre", "colour"]
 
 type board_flat = list[Cell_]
 type board_matrix = list[list[Cell_]]
+
+#_ type board_flat = list[Annotated[object, 'Cell']]
+#_ type board_matrix = list[list[Annotated[object, 'Cell']]]
 
 type board_flat_raw = Iterable[int]
 type board_matrix_raw = Iterable[Iterable[int]]
