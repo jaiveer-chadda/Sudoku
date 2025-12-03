93c240d84bb5922ed0cb2d05c0574cd268e3290d
JV <jaiveer.chadda@gmail.com>
2025-12-03 17:04:07 +0000

    Created a Dimensions dataclass, and refactored file to use it

---
+++ source/gui/dimensions.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from typing import Optional, Generator
+
+
+class NotSquareError(Exception):
+    def __init__(self, obj: Dimension) -> None:
+        super().__init__(f"{obj.__repr__()} is not a square.")
+
+
+@dataclass
+class Dimension:
+    width: int
+    height: Optional[int] = None
+    
+    def __post_init__(self) -> None:
+        # if only one value is entered, or if the values are the same
+        #   assume the dimensions are a square (nxn)
+        self.is_square: bool = (self.width == self.height) or (self.height is None)
+        
+        if self.is_square:
+            self.height = self.width
+    
+    @property
+    def dimension(self) -> int:
+        if not self.is_square:
+            raise NotSquareError(self)
+        return self.width
+    
+    @property
+    def x(self) -> int:
+        return self.width
+    
+    @property
+    def y(self) -> int:
+        return self.height
+    
+    @property
+    def area(self) -> int:
+        return self.width * self.height
+    
+    @property
+    def count(self) -> int:
+        return self.area
+    
+    @property
+    def largest(self) -> int:
+        return max(self.width, self.height)
+    
+    @property
+    def smallest(self) -> int:
+        return min(self.width, self.height)
+    
+    def __repr__(self) -> str:
+        return f"Dimensions(width|x={self.width}, height|y={self.height})"
+    
+    def __iter__(self) -> Generator[int, None, None]:
+        for value in (self.width, self.height):
+            yield value
--- source/gui/main.py
+++ source/gui/main.py
@@ -1,53 +1,69 @@
 import tkinter as tk
 
+from source.gui.dimensions import Dimension
+
 APP_TITLE: str = "Sudoku App"
 
-WINDOW_SIZE_PX: tuple[int, int] = (600, 800)
-_WINDOW_WIDTH_PX, _WINDOW_HEIGHT_PX = WINDOW_SIZE_PX
+WINDOW_SIZE:      Dimension = Dimension(600, 800)
+WINDOW_OFFSET:    Dimension = Dimension(0, -50)
 
-BOARD_DIMS_PX: int = 500
-
-BOARD_XY_SIZE: int = 9
+BOARD_SIZE_PX:    Dimension = Dimension(500)
+BOARD_SIZE_CELLS: Dimension = Dimension(9)
 
 
 class AppGUI:
-    def __init__(self, title: str = APP_TITLE) -> None:
+    def __init__(self,
+                 title: str = APP_TITLE,
+                 width: int = WINDOW_SIZE.width,
+                 height: int = WINDOW_SIZE.height,
+                 offset_x: int = WINDOW_OFFSET.x,
+                 offset_y: int = WINDOW_OFFSET.y
+                 ) -> None:
         self.title: str = title
+        
+        self.size_px: Dimension = Dimension(width, height)
+        self._offset: Dimension = Dimension(offset_x, offset_y)
+        
         self._window_init()
 
-    def _window_init(self, width: int = _WINDOW_WIDTH_PX, height: int = _WINDOW_HEIGHT_PX) -> None:
-        self.window_width: int = width
-        self.window_height: int = height
+    def _window_init(self) -> None:
+        self.root_window: tk.Tk = tk.Tk()
         
-        self.window: tk.Tk = tk.Tk()
         self._set_window_dimensions_and_pos()
-        self.window.resizable(False, False)
-        self.window.title("Sudoku App")
+        self.root_window.resizable(False, False)
+        self.root_window.title(self.title)
 
     def _set_window_dimensions_and_pos(self) -> None:
-        x: int = (self.window.winfo_screenwidth() - self.window_width) // 2
-        y: int = (self.window.winfo_screenheight() - self.window_height) // 2
-        self.window.geometry(f"{self.window_width}x{self.window_height}+{x}+{y-50}")
+        _screen_width: int = self.root_window.winfo_screenwidth()
+        _screen_height: int = self.root_window.winfo_screenheight()
+        
+        _window_centre_pos_x: int = (_screen_width - self.size_px.width) // 2
+        _window_centre_pos_y: int = (_screen_height - self.size_px.height) // 2
+        
+        self.root_window.geometry(
+            f"{self.size_px.width}x{self.size_px.height}" +
+            f"+{_window_centre_pos_x+self._offset.x}+{_window_centre_pos_y++self._offset.y}"
+        )
 
 
 app: AppGUI = AppGUI()
 
-board_container: tk.Frame = tk.Frame(app.window)
+board_container: tk.Frame = tk.Frame(app.root_window)
 board_container.pack(expand=True, fill='both')
 
 board_frame: tk.Frame = tk.Frame(board_container, background="red")
 
-# relx=0.5 places the box in the centre horizontally, and y=50 places it 50 px (padding) away from the top
-board_frame.place(relx=0.5, y=50, anchor="n", width=BOARD_DIMS_PX, height=BOARD_DIMS_PX)
+# relx=0.5 places the box in the centre horizontally, and y=50 places it 50 px away from the top (as padding)
+board_frame.place(relx=0.5, y=50, anchor="n", width=BOARD_SIZE_PX.width, height=BOARD_SIZE_PX.height)
 
 # set grid weights for equal column and row spacing
-for row_col in range(BOARD_XY_SIZE):
+for row_col in range(BOARD_SIZE_CELLS.dimension):
     board_frame.rowconfigure(row_col, weight=1)
     board_frame.columnconfigure(row_col, weight=1)
 
-for cell_index in range(BOARD_XY_SIZE**2):
-    row, col = cell_index//BOARD_XY_SIZE, cell_index%BOARD_XY_SIZE
-    
+for cell_index in range(BOARD_SIZE_CELLS.dimension**2):
+    row, col = cell_index//BOARD_SIZE_CELLS.dimension, cell_index%BOARD_SIZE_CELLS.dimension
+
     cell_frame_i: tk.Frame = tk.Frame(master=board_frame, borderwidth=1, background='green')
     cell_frame_i.grid(row=row, column=col, sticky=tk.NSEW)  # sticky=tk.NSEW makes each cell expand to fill the frame
     cell_frame_i.grid_propagate(False)
@@ -55,4 +71,4 @@ for cell_index in range(BOARD_XY_SIZE**2):
     debug_lab: tk.Label = tk.Label(cell_frame_i, text=f"({col + 1},{row + 1})")
     debug_lab.pack(expand=True, fill='both')
 
-app.window.mainloop()
+app.root_window.mainloop()
