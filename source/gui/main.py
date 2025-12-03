e67bc6c2c4886114c6d258995901fddba87a344f
JV <jaiveer.chadda@gmail.com>
2025-12-03 15:01:15 +0000

    Refactored GUI file for clarity and conciseness

--- source/gui/main.py
+++ source/gui/main.py
@@ -1,24 +1,27 @@
 import tkinter as tk
-from tkinter import Tk, Label, Canvas, Frame, Button
-
-
-WINDOW_WIDTH: int = 600
-WINDOW_HEIGHT: int = 800
 
 APP_TITLE: str = "Sudoku App"
+
+WINDOW_SIZE_PX: tuple[int, int] = (600, 800)
+BOARD_DIMS_PX: int = 500
+
+BOARD_XY_SIZE: int = 9
+
+
+_WINDOW_WIDTH_PX: int = WINDOW_SIZE_PX[0]
+_WINDOW_HEIGHT_PX: int = WINDOW_SIZE_PX[1]
 
 
 class AppGUI:
     def __init__(self, title: str = APP_TITLE) -> None:
         self.title: str = title
         self._window_init()
-        # self._canvas_init()
 
-    def _window_init(self, width: int = WINDOW_WIDTH, height: int = WINDOW_HEIGHT) -> None:
+    def _window_init(self, width: int = _WINDOW_WIDTH_PX, height: int = _WINDOW_HEIGHT_PX) -> None:
         self.window_width: int = width
         self.window_height: int = height
         
-        self.window: Tk = Tk()
+        self.window: tk.Tk = tk.Tk()
         self._set_window_dimensions_and_pos()
         self.window.resizable(False, False)
         self.window.title("Sudoku App")
@@ -27,44 +30,32 @@ class AppGUI:
         x: int = (self.window.winfo_screenwidth() - self.window_width) // 2
         y: int = (self.window.winfo_screenheight() - self.window_height) // 2
         self.window.geometry(f"{self.window_width}x{self.window_height}+{x}+{y-50}")
-        
-    # def _canvas_init(self) -> None:
-    #     self.canvas: Canvas = Canvas(self.window, width=self.window_width, height=self.window_height)
-    #     self.canvas.pack()
 
 
 app: AppGUI = AppGUI()
 
-board_dims: int = 500
+board_container: tk.Frame = tk.Frame(app.window)
+board_container.pack(expand=True, fill='both')
 
-
-outer = Frame(app.window)
-outer.pack(expand=True, fill='both')
-
-test_frame = Frame(outer, background="red")
-test_frame.place(relx=0.5, y=50, anchor="n", width=board_dims, height=board_dims)
-
-rows: int = 9
-cols: int = 9
+board_frame: tk.Frame = tk.Frame(board_container, background="red")
+board_frame.place(relx=0.5, y=50, anchor="n", width=BOARD_DIMS_PX, height=BOARD_DIMS_PX)
 
 # set grid weights for equal spacing
-for i in range(rows):
-    test_frame.rowconfigure(i, weight=1)
-for j in range(cols):
-    test_frame.columnconfigure(j, weight=1)
+for row_col in range(BOARD_XY_SIZE):
+    board_frame.rowconfigure(row_col, weight=1)
+    board_frame.columnconfigure(row_col, weight=1)
 
-for i in range(rows):
-    for j in range(cols):
-        frame = Frame(
-            master=test_frame,
+for row in range(BOARD_XY_SIZE):
+    for col in range(BOARD_XY_SIZE):
+        cell_frame: tk.Frame = tk.Frame(
+            master=board_frame,
             borderwidth=1,
             background='green'
         )
-        frame.grid(row=i, column=j, sticky='nsew')  # sticky = expand to fill the cell
-        frame.grid_propagate(False)
+        cell_frame.grid(row=row, column=col, sticky=tk.NSEW)  # sticky makes it expand to fill the frame
+        cell_frame.grid_propagate(False)
         
-        lab = Label(frame, text=f"({j+1},{i+1})")
-        lab.pack(expand=True, fill='both')
-
+        debug_lab: tk.Label = tk.Label(cell_frame, text=f"({col + 1},{row + 1})")
+        debug_lab.pack(expand=True, fill='both')
 
 app.window.mainloop()
