ed3636592a984aa4a643c9b00229166c167d698f
JV <jaiveer.chadda@gmail.com>
2025-12-03 15:10:55 +0000

    Refactored code for simplicity and added comments

--- source/gui/main.py
+++ source/gui/main.py
@@ -3,13 +3,11 @@ import tkinter as tk
 APP_TITLE: str = "Sudoku App"
 
 WINDOW_SIZE_PX: tuple[int, int] = (600, 800)
+_WINDOW_WIDTH_PX, _WINDOW_HEIGHT_PX = WINDOW_SIZE_PX
+
 BOARD_DIMS_PX: int = 500
 
 BOARD_XY_SIZE: int = 9
-
-
-_WINDOW_WIDTH_PX: int = WINDOW_SIZE_PX[0]
-_WINDOW_HEIGHT_PX: int = WINDOW_SIZE_PX[1]
 
 
 class AppGUI:
@@ -38,24 +36,23 @@ board_container: tk.Frame = tk.Frame(app.window)
 board_container.pack(expand=True, fill='both')
 
 board_frame: tk.Frame = tk.Frame(board_container, background="red")
+
+# relx=0.5 places the box in the centre horizontally, and y=50 places it 50 px (padding) away from the top
 board_frame.place(relx=0.5, y=50, anchor="n", width=BOARD_DIMS_PX, height=BOARD_DIMS_PX)
 
-# set grid weights for equal spacing
+# set grid weights for equal column and row spacing
 for row_col in range(BOARD_XY_SIZE):
     board_frame.rowconfigure(row_col, weight=1)
     board_frame.columnconfigure(row_col, weight=1)
 
-for row in range(BOARD_XY_SIZE):
-    for col in range(BOARD_XY_SIZE):
-        cell_frame: tk.Frame = tk.Frame(
-            master=board_frame,
-            borderwidth=1,
-            background='green'
-        )
-        cell_frame.grid(row=row, column=col, sticky=tk.NSEW)  # sticky makes it expand to fill the frame
-        cell_frame.grid_propagate(False)
-        
-        debug_lab: tk.Label = tk.Label(cell_frame, text=f"({col + 1},{row + 1})")
-        debug_lab.pack(expand=True, fill='both')
+for cell_index in range(BOARD_XY_SIZE**2):
+    row, col = cell_index//BOARD_XY_SIZE, cell_index%BOARD_XY_SIZE
+    
+    cell_frame_i: tk.Frame = tk.Frame(master=board_frame, borderwidth=1, background='green')
+    cell_frame_i.grid(row=row, column=col, sticky=tk.NSEW)  # sticky=tk.NSEW makes each cell expand to fill the frame
+    cell_frame_i.grid_propagate(False)
+    
+    debug_lab: tk.Label = tk.Label(cell_frame_i, text=f"({col + 1},{row + 1})")
+    debug_lab.pack(expand=True, fill='both')
 
 app.window.mainloop()
