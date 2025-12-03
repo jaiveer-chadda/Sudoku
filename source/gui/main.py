cb65071c2789e3cc3b7953ebabfa8e07a73bb1cd
JV <jaiveer.chadda@gmail.com>
2025-12-03 17:38:43 +0000

    Put most of the GUI functionality into classes and functions For ease of use and reading

--- source/gui/main.py
+++ source/gui/main.py
@@ -2,13 +2,16 @@ import tkinter as tk
 
 from source.gui.dimensions import Dimension
 
+
 APP_TITLE: str = "Sudoku App"
 
 WINDOW_SIZE:      Dimension = Dimension(600, 800)
-WINDOW_OFFSET:    Dimension = Dimension(0, -50)
+WINDOW_OFFSET:    Dimension = Dimension(0, -30)
 
 BOARD_SIZE_PX:    Dimension = Dimension(500)
 BOARD_SIZE_CELLS: Dimension = Dimension(9)
+
+BOARD_PADDING_TOP: int = 50
 
 
 class AppGUI:
@@ -42,33 +45,74 @@ class AppGUI:
         
         self.root_window.geometry(
             f"{self.size_px.width}x{self.size_px.height}" +
-            f"+{_window_centre_pos_x+self._offset.x}+{_window_centre_pos_y++self._offset.y}"
+            f"+{_window_centre_pos_x+self._offset.x}+{_window_centre_pos_y+self._offset.y}"
         )
+        
+        
+class BoardGUI:
+    def __init__(self, master: AppGUI, padding_top: int = BOARD_PADDING_TOP, background: str = "") -> None:
+        self.master: AppGUI = master
+        
+        self._padding_top: int = padding_top
+        self._background: str = background  #?bg=red for debugging
+        
+        self._cells: list[CellGUI] = [].copy()
+        
+        self._container_init()
+        self._board_init()
+        self._init_all_cells()
+    
+    def _container_init(self) -> None:
+        self.container: tk.Frame = tk.Frame(self.master.root_window)
+        self.container.pack(expand=True, fill='both')
+        
+    def _board_init(self) -> None:
+        def _configure_board_grid_spacing() -> None:
+            # set grid weights for equal column and row spacing
+            for _row_col in range(BOARD_SIZE_CELLS.dimension):
+                self.frame.rowconfigure(_row_col, weight=1)
+                self.frame.columnconfigure(_row_col, weight=1)
+        
+        self.frame: tk.Frame = tk.Frame(self.container, background=self._background)
+        # anchor='n' means that when setting the frame's pos, I'm focusing on the top middle part of it to place
+        # relx=0.5 places the anchor in the centre horizontally, and y=50 places it 50px away from the top (as padding)
+        self.frame.place(
+            relx=0.5, y=self._padding_top,
+            anchor="n",
+            width=BOARD_SIZE_PX.width, height=BOARD_SIZE_PX.height
+        )
+        _configure_board_grid_spacing()
+            
+    def _init_all_cells(self) -> None:
+        for cell_index in range(BOARD_SIZE_CELLS.dimension**2):
+            new_cell: CellGUI = CellGUI(self, cell_index)
+            self._cells.append(new_cell)
+
+
+class CellGUI:
+    def __init__(self, master: BoardGUI, index: int) -> None:
+        self.master: BoardGUI = master
+        self.index: int = index
+        
+        self.row: int = index // BOARD_SIZE_CELLS.dimension
+        self.col: int = index % BOARD_SIZE_CELLS.dimension
+        
+        self._cell_init()
+        self._add_debug_label()
+        
+    def _cell_init(self) -> None:
+        self.frame: tk.Frame = tk.Frame(master=self.master.frame, borderwidth=1, background='green')
+        
+        # sticky=tk.NSEW makes each cell expand to fill the frame
+        self.frame.grid(row=self.row, column=self.col, sticky=tk.NSEW)
+        self.frame.grid_propagate(False)
+
+    def _add_debug_label(self) -> None:
+        debug_lab: tk.Label = tk.Label(self.frame, text=f"({self.col + 1},{self.row + 1})")
+        debug_lab.pack(expand=True, fill='both')
 
 
 app: AppGUI = AppGUI()
-
-board_container: tk.Frame = tk.Frame(app.root_window)
-board_container.pack(expand=True, fill='both')
-
-board_frame: tk.Frame = tk.Frame(board_container, background="red")
-
-# relx=0.5 places the box in the centre horizontally, and y=50 places it 50 px away from the top (as padding)
-board_frame.place(relx=0.5, y=50, anchor="n", width=BOARD_SIZE_PX.width, height=BOARD_SIZE_PX.height)
-
-# set grid weights for equal column and row spacing
-for row_col in range(BOARD_SIZE_CELLS.dimension):
-    board_frame.rowconfigure(row_col, weight=1)
-    board_frame.columnconfigure(row_col, weight=1)
-
-for cell_index in range(BOARD_SIZE_CELLS.dimension**2):
-    row, col = cell_index//BOARD_SIZE_CELLS.dimension, cell_index%BOARD_SIZE_CELLS.dimension
-
-    cell_frame_i: tk.Frame = tk.Frame(master=board_frame, borderwidth=1, background='green')
-    cell_frame_i.grid(row=row, column=col, sticky=tk.NSEW)  # sticky=tk.NSEW makes each cell expand to fill the frame
-    cell_frame_i.grid_propagate(False)
-    
-    debug_lab: tk.Label = tk.Label(cell_frame_i, text=f"({col + 1},{row + 1})")
-    debug_lab.pack(expand=True, fill='both')
+board: BoardGUI = BoardGUI(app)
 
 app.root_window.mainloop()
