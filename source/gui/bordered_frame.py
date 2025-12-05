f6e9aeac9b2d4085d4f6a3cc5acfb0df1669bfc2
JV <jaiveer.chadda@gmail.com>
2025-12-05 02:12:26 +0000

    Implemented methods to change border colour and weight

--- source/gui/bordered_frame.py
+++ source/gui/bordered_frame.py
@@ -1,5 +1,6 @@
 #—— External Imports —————————————————————————————————————————————————————————————————————————
 import tkinter as tk
+from typing import Literal
 
 #—— Project Imports ——————————————————————————————————————————————————————————————————————————
 #————— GUI ——————————————————————————————————————
@@ -21,12 +22,29 @@ class BorderedFrame(tk.Frame):
             interior_widget: type[tk.Widget] = tk.Frame,
             **kwargs
     ) -> None:
-        super().__init__(master, background=border_colour, bd=0, highlightthickness=0)
+        self.border_weights: Directions = border_weights
+        self.border_colour: str = border_colour
         
+        super().__init__(master, background=self.border_colour, bd=0, highlightthickness=0)
+        
+        # Create an instance of whichever object was passed into interior_widget
         self.interior: tk.Widget = interior_widget.__new__(interior_widget)
+        # Initialise that object
         self.interior.__init__(self, **kwargs)
         
         self.interior.pack(
-            padx=(border_weights.left, border_weights.right),
-            pady=(border_weights.top, border_weights.bottom)
+            padx=(self.border_weights.left, self.border_weights.right),
+            pady=(self.border_weights.top, self.border_weights.bottom)
         )
+        
+    def change_border_colour(self, colour: str) -> None:
+        self.border_colour = colour
+        
+    def change_border_weights(self, weights: Directions, mode: Literal['add', 'absolute'] = 'absolute') -> None:
+        match mode:
+            case 'add':
+                self.border_weights: Directions = self.border_weights + weights
+            case 'absolute' | _:
+                self.border_weights: Directions = weights
+            
+    
